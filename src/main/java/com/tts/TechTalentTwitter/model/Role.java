c08d1fe56126b905ba2860636142d029568770bc
Rosesaletha Scott <rosesalesscott@gmail.com>
2020-06-18 12:24:52 -0400

    First commit

---
+++ src/main/java/com/tts/TechTalentTwitter/model/Role.java
@@ -0,0 +1,35 @@
+
+package com.tts.TechTalentTwitter.model;
+
+
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+    private String role;
+	public Role(String role) {
+		super();
+		this.role = role;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "Role [role=" + role + "]";
+	}
+}
---
+++ src/main/java/com/tts/TechTalentTwitter/model/User.java
@@ -0,0 +1,153 @@
+package com.tts.TechTalentTwitter.model;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+    
+    @Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
+    private String email;
+    
+    @NotEmpty(message = "Please provide a username")
+    @Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp="[^\\s]+", message="Your username cannot contain spaces")
+    private String username;
+    
+    @Length(min = 5, message = "Your password must have at least 5 characters")
+    @NotEmpty(message = "Please provide a password")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private String password;
+    
+    @NotEmpty(message = "Please provide your first name")
+    private String firstName;
+    
+    @NotEmpty(message = "Please provide your last name")
+    private String lastName;
+    
+    private int active;
+    
+    @CreationTimestamp 
+	private Date createdAt;
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+    
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    private List<User> followers;
+    
+    @ManyToMany(mappedBy="followers")
+	private List<User> following;
+
+    public User() {};
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getActive() {
+		return active;
+	}
+	public void setActive(int active) {
+		this.active = active;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	public List<User> getFollowers() {
+		return followers;
+	}
+	public void setFollowers(List<User> followers) {
+		this.followers = followers;
+	}
+	public List<User> getFollowing() {
+		return following;
+	}
+	public User(
+			@Email(message = "Please provide a valid email") @NotEmpty(message = "Please provide an email") String email,
+			@NotEmpty(message = "Please provide a username") @Length(min = 3, message = "Your username must have at least 3 characters") @Length(max = 15, message = "Your username cannot have more than 15 characters") @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces") String username,
+			@Length(min = 5, message = "Your password must have at least 5 characters") @NotEmpty(message = "Please provide a password") String password,
+			@NotEmpty(message = "Please provide your first name") String firstName,
+			@NotEmpty(message = "Please provide your last name") String lastName, int active, Date createdAt,
+			Set<Role> roles, List<User> followers, List<User> following) {
+		super();
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.active = active;
+		this.roles = roles;
+		this.followers = followers;
+		this.following = following;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", username=" + username + ", password=" + password
+				+ ", firstName=" + firstName + ", lastName=" + lastName + ", active=" + active + ", createdAt="
+				+ createdAt + ", followers=" + followers + ", following=" + following + "]";
+	}
+
+    
+}
+
